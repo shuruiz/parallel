a9addcb5f912942eaf881de975c3e7a4253d954d
Ethan <zhangzhbd@gmail.com>
2018-10-01 20:44:42 -0400

    initialization

---
+++ flux_parallel/flux_parallel/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  flux_parallel
+//
+//  Created by Ethan Zhang on 10/1/18.
+//  Copyright © 2018 Ethan Zhang. All rights reserved.
+//
+
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
