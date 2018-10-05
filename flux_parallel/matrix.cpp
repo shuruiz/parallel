1fb1249ecdaeb74a6764e17ed2fe2e2a6a768035
Ethan <zhangzhbd@gmail.com>
2018-10-05 15:00:32 -0400

    mean variable z

--- flux_parallel/matrix.cpp
+++ flux_parallel/matrix.cpp
@@ -10,6 +10,7 @@ n=500;
 p =1; // # of proc. 
 int main(int argc, char** argv){
 	
+	MPI_BARRIER(0)
 	double starttime, endtime;
 	starttime = MPI_Wtime();
 	MPT_INITE()
@@ -19,16 +20,28 @@ int main(int argc, char** argv){
 			// initialize below 
 			A[i][j] = i* sin(i) +j * cos(j) + sqrt(i+j);			
 		}
+
+	// iteration 
 	}
 
-
+/*
 	double A_0[m][n];
 
 	for(i=0; i<=m; i++){
 		for(j=0;j<=n;j++){
 			A_0 = A;
+			if(i==0 || i ==m-1 || j==0||j==n-1){
+				A[i][j] = A_0 [i][j];  //unchanged along boarder
+			}
+
+			else{
+				z = (f(A_0[i-1][j])+f(A_0[i+1][j])+f(A_0[i][j-1]) + f(A_0[i][j+1]) + f(A_0[i][j])) / 5;
+				A[i][j] = max(-100, min(100, z)); 
+			}
 		}
 	}
+
+	*/ 
 
 	endtime = MPI_Wtime();
 
