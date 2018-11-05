9c4e8239fc43c766d92387ce12bb6dd39db7cb44
Ethan <zhangzhbd@gmail.com>
2018-11-04 22:02:59 -0500

    main.cpp

--- pittsburgh_bridge/main.cpp
+++ pittsburgh_bridge/main.cpp
@@ -17,30 +17,30 @@ double fx(double x);
 double M= -DBL_MAX; // initialize M to the -max_double, infinite small
 int _current=0; // threads
 
-omp_lock_t maxLock;
-omp_lock_t stackLock;
+omp_lock_t masterlock;
+omp_lock_t workerlock;
 
 int main(int argc, char** argv)
 {
     
     _stack.push(make_pair(1, 100));
     double starttime=omp_get_wtime();
-    omp_init_lock(&maxLock);
-    omp_init_lock(&stackLock);
+    omp_init_lock(&masterlock);
+    omp_init_lock(&workerlock);
     
 #pragma omp parallel
     {
         while(true)
         {
-            omp_set_lock(&stackLock);
+            omp_set_lock(&workerlock);
             // elimiate cout command to shorten the running time.
             // cout<<"on thread: "<<omp_get_thread_num()<<endl;
             if(_stack.empty()){ // assign work
                 if(_current==0){
-                    omp_unset_lock(&stackLock);
+                    omp_unset_lock(&workerlock);
                     break;}
                 else
-                    omp_unset_lock(&stackLock);
+                    omp_unset_lock(&workerlock);
             }
             else{
                 _current++;
@@ -48,12 +48,12 @@ int main(int argc, char** argv)
                 double d=_stack.top().second; //get right of the interval
       
                 _stack.pop();
-                omp_unset_lock(&stackLock);
+                omp_unset_lock(&workerlock);
                 
                 double f_c=fx(c);
                 double f_d=fx(d);
                 
-                omp_set_lock(&maxLock);
+                omp_set_lock(&masterlock);
                 
                 if(f_c>M || f_d>M)
                 {
@@ -63,22 +63,22 @@ int main(int argc, char** argv)
                 
                 if(((f_c+f_d+s*(d-c))/2)>=M+epsilon) // computing the max of interval [c,d]
                 {
-                    omp_set_lock(&stackLock);
+                    omp_set_lock(&workerlock);
                     // breadth-first generating intervals
                     _stack.push(make_pair(c, (c+d)/2));
                     _stack.push(make_pair((c+d)/2, d));
                     _current--;
-                    omp_unset_lock(&stackLock);
+                    omp_unset_lock(&workerlock);
                 }
                 
                 else
                 //get rid of this interval, no need to compute because max interval [a,b] < M+epsilon
                 {
-                    omp_set_lock(&stackLock);
+                    omp_set_lock(&workerlock);
                     _current--;
-                    omp_unset_lock(&stackLock);
+                    omp_unset_lock(&workerlock);
                 }
-                omp_unset_lock(&maxLock);
+                omp_unset_lock(&masterlock);
             }
         }
     }
