c5a477fd88fdd88b280d83c6625f631787e7127e
Ethan <zhangzhbd@gmail.com>
2018-11-10 17:26:53 -0500

    stencil 2d with cuda

--- flux_gpu/stencil/stencil/main.cpp
+++ flux_gpu/stencil/stencil/main.cpp
@@ -9,55 +9,50 @@
 
 #include <iostream>
 #include <algorithm>
+#include <vector>
+#include "cmath"
 using namespace std;
-#define N 1024
-#define RADIUS 3
-#define BLOCK_SIZE 16
+#define N (1024*1024)
+#define THREADS_PER_BLOCK 512
 
 
-__global__ void stencil_1d(int *in, int *out) {
-    __shared__ int temp[BLOCK_SIZE + 2 * RADIUS];
-    int gindex = threadIdx.x + blockIdx.x * blockDim.x;
-    int lindex = threadIdx.x + RADIUS;
-    // Read input elements into shared memory
-    temp[lindex] = in[gindex];
-    if (threadIdx.x < RADIUS) {
-        temp[lindex - RADIUS] = in[gindex - RADIUS];
-        temp[lindex + BLOCK_SIZE] = in[gindex + BLOCK_SIZE];
+__global__ void stencil_1d(double *a, double *b, double *c, int n) {
+    
+}
+
+
+int main(int argc, char** argv) {
+    std::vector<std::vector<double> > array;
+    // initialize below
+    int n = *argv[1];
+    double array(n,n);
+    
+    for(int i =0; i<n;i++){
+        for(int j =0; j<n; j++){
+            array[i][j] = pow(1+cos(2*i)+sin(j),2);
+        }
     }
-    // Synchronize (ensure all the data is available)
-    __syncthreads();
-    // Apply the stencil
-    int result = 0;
-    for (int offset = -RADIUS ; offset <= RADIUS ; offset++)
-        result += temp[lindex + offset];
-    // Store the result
-    out[gindex] = result;
-}
-
-
-void fill_ints(int *x, int n) {
-    fill_n(x, n, 1);
-}
-int main(void) {
-    int *in, *out; // host copies of a, b, c
-    int *d_in, *d_out; // device copies of a, b, c
-    int size = (N + 2*RADIUS) * sizeof(int);
-    // Alloc space for host copies and setup values
-    in = (int *)malloc(size); fill_ints(in, N + 2*RADIUS);
-    out = (int *)malloc(size); fill_ints(out, N + 2*RADIUS);
-    // Alloc space for device copies
-    cudaMalloc((void **)&d_in, size);
-    cudaMalloc((void **)&d_out, size);
-    // Copy to device
-    cudaMemcpy(d_in, in, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_out, out, size, cudaMemcpyHostToDevice);
-    // Launch stencil_1d() kernel on GPU
-    stencil_1d<<<N/BLOCK_SIZE,BLOCK_SIZE>>>(d_in + RADIUS, d_out + RADIUS);
+    
+    double *a, *b, *c;
+    double *d_a, *d_b, *d_c;
+    int size = N * sizeof(int);
+    
+    // allocate memory on device
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_a, size);
+    cudaMalloc((void **)&d_a, size);
+    
+    // Copy inputs to device
+    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    
+    stencil_1d<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a, d_b, d_c);
+    
     // Copy result back to host
-    cudaMemcpy(out, d_out, size, cudaMemcpyDeviceToHost);
-    // Cleanup
-    free(in); free(out);
-    cudaFree(d_in); cudaFree(d_out);
+    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    
+    free(a); free(b); free(c);
+    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+  
     return 0;
 }
