222dfd8d8e37c1444955ef2cc5e7e9f0ab4dbb10
Ethan <zhangzhbd@gmail.com>
2018-11-11 18:58:01 -0500

    Add pbs batch file and change array to 1d

--- flux_gpu/stencil/stencil/main.cpp
+++ flux_gpu/stencil/stencil/main.cpp
@@ -16,13 +16,16 @@
 #include <float.h>
 #include "cuda.h"
 
-#define THREADS_PER_BLOCK 512
+#define THREADS_PER_BLOCK 256
 
 using namespace std;
-vector<vector<double> > calc(int i, int j, int n, vector<vector<double> > arr){
+
+__global__
+void calc(int i, int j, int n, double *A, double *prev_A){
     double first,second;
     first = second = DBL_MAX;
     if(i ==0 || i ==n-1 || j ==0 || j ==n-1){ // unchanged, do nothing
+        A[i*n+j] = prev_A[i*n+j];
     }
     else{
         //assign secondMin to A[i][j]
@@ -37,76 +40,113 @@ vector<vector<double> > calc(int i, int j, int n, vector<vector<double> > arr){
         }
     }
     arr[i][j] += second;
-    return arr;
 }
 
-double verisum_all(vector<vector<double> > A){
+
+double verisum_all(int n, double *A){
     double sum=0.0;
-    for(int i = 0; i<A.size(); i++){
-        for(int j=0; j<A[i].size(); j++){
-            sum += A[i][j];
+    for(int i = 0; i<n; i++){
+        for(int j=0; j<n; j++){
+            sum += A[i*n+ j];
         }
     }
     return sum;
 }
 
-double value_half(vector<vector<double> > A, int n){
+double value_half(int n, double *A){
     int fl = floor(n/2);
-    return A[fl][fl];
+    return A[fl * n + fl];
 }
 
-double value_37_47(vector<vector<double> > A){
-    return A[37][47];
+double value_37_47(int n, double *A){
+    return A[37*n + 47];
 }
 
 __global__
-void stencil_2d(char **arr, int n) {
-    int index_i = floor(threadIdx.x + blockIdx.x * THREADS_PER_BLOCK / (n*1.0));
-    int index_j = floor(threadIdx.x + blockIdx.x * THREADS_PER_BLOCK % n);
-    arr = calc(index_i, index_j, n, arr);
+void stencil(double *A, double *prev_dA, int n) {
+    int index = threadIdx.x + blockIdx.x * THREADS_PER_BLOCK;
+    int i = floor(index / n);
+    int j = index % n;
+    
+    double first, second;
+    first = second = DBL_MAX;
+    if(i ==0 || i ==n-1 || j ==0 || j ==n-1){ // unchanged, do nothing
+        A[i*n+j] = prev_A[i*n+j];
+    }
+    else{
+        double tmp[] = {A[(i+1)*n+ (j+1)], A[(i+1)*n+(j-1)],A[(i-1)*n +(j+1)],A[(i-1)*n + (j-1)]};
+        for(int k =0; k<4; k++){
+            if(tmp[k]<first){
+                second = first;
+                first = tmp[k];
+            }
+            else if (tmp[k] < second && tmp[k] != first){
+                second = tmp[k];}
+        }
+        A[i*n+j] += second;
+    }
+    prev_dA = A
 }
 
+void compute(double *dA, double *prev_dA, int n, int t){
+    for(int episode = 0; episode <t; episode++){
+        int N = n*n;
+        stencil<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(dA, prev_dA, n);
+    }
+}
 
 int main(int argc, char** argv) {
     // initialize below
     int n = *argv[1];
-    std::vector<double> v1(n);
-    std::vector<std::vector<double> > array(n,v1);
-    for(int i =0; i<array.size();i++){
-        for(int j =0; j<array[i].size(); j++){
-            array[i][j] = pow(1+cos(2*i)+sin(j),2);
+    int N  = n*n;
+    
+//2d stencil, represented by 1d stencil
+    // initialize below
+    double *array;
+    int size = N * sizeof(double);
+    array =(double *)malloc(N);
+    for(int i =0; i<n;i++){
+        for(int j =0; j<n; j++){
+            array[i*n+j] = pow(1+cos(2*i)+sin(j),2);
         }
     }
+    
+    
     // variables
-    double *a, *b, *c, *d;
-    double *d_a, *d_b, *d_c, *d_d;
-    int N  = n*n;
-    int size = N * sizeof(double);
+//    double *a, *b, *c, *d;
+//    double *d_a, *d_b, *d_c, *d_d;
     
+    
+    double *dA;
+    double *prev_dA;  // time t-1 matrix
     // allocate memory on device
-    cudaMalloc((void **)&d_a, size);
-    cudaMalloc((void **)&d_b, size);
-    cudaMalloc((void **)&d_c, size);
-    cudaMalloc((void **)&d_d, size);
-    
-    //allocate memory on host
-//    a = (double *)malloc(size);
-//    b = (double *)malloc(size);
-//    c = (double *)malloc(size);
-//    d = (double *)malloc(size);
+    cudaMalloc((void **)&dA, size);
+    cudaMalloc((void **)&prev_dA, size);
     
     // Copy inputs to device
-    cudaMemcpy(d_a, a, size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b, b, size, cudaMemcpyHostToDevice);
+    cudaMemcpy(dA, array, size, cudaMemcpyHostToDevice);
+//    cudaMemcpy(prev_dA, array, size, cudaMemcpyHostToDevice);
     
     //launch kernal on device
-    stencil_2d<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a,d_b, d_c);
+//    stencil<<<N/THREADS_PER_BLOCK,THREADS_PER_BLOCK>>>(d_a,d_b, d_c);
+    int t  = 10;
+    compute(dA, prev_dA, n, t)
     
     // Copy result back to host
-    cudaMemcpy(c, d_c, size, cudaMemcpyDeviceToHost);
+    cudaMemcpy(array, dA, size, cudaMemcpyDeviceToHost);
+    double verisum = verisum_all(n, array);
+    double half_value = value_half(n, array);
+    double spec  = value_37_47(n, array);
     
-//    free(a); free(b); free(c);
-    cudaFree(d_a); cudaFree(d_b); cudaFree(d_c);
+    //print result
+    printf("verisum all %f/n", verisum);
+    printf("verisum n/2 %f/n", half_value);
+    printf("verisum A[37][47] %f/n", spec);
+    
+    
+    //free memory
+    free(array);
+    cudaFree(dA); cudaFree(prev_dA);
   
     return 0;
 }
