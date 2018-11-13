dab803547a2906703ca0063332051e73d89b8282
Ethan <zhangzhbd@gmail.com>
2018-11-12 23:11:45 -0500

    merge

--- flux_gpu/stencil/stencil/unshared.cu
+++ flux_gpu/stencil/stencil/unshared.cu
@@ -101,14 +101,6 @@ int main(int argc, const char * argv[]) {
     dim3 dimGrid(matrix_size / BLOCK_SIZE, matrix_size / BLOCK_SIZE);
 
     cout << "here3" << endl;
-<<<<<<< HEAD
-    double starttime = time(0);
-    for (int i = 0; i < 10; i++) {
-        MatrixUpdate<<<dimGrid, dimBlock>>>(d_M1, d_store);
-    }
-    double endtime = time(0);
-    printf("total time %f\n", (starttime - endtime));
-=======
     clock_t t; 
     t = clock();
     for (int i = 0; i < 10; i++) {
@@ -116,7 +108,6 @@ int main(int argc, const char * argv[]) {
     }
     t = clock()-t;
     printf("total time %f\n", ((float)t)/CLOCKS_PER_SEC);
->>>>>>> 1035f61bf3f580f5b105d56589f8d52aeaddd302
     cout << "here4" << endl;
     
     Matrix res;
