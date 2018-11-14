924facf280c1f7bbdcb9af1253babf4ff15171da
Ethan <zhangzhbd@gmail.com>
2018-11-13 23:50:31 -0500

    seg fault 11

--- flux_gpu/stencil/stencil/serial.cpp
+++ flux_gpu/stencil/stencil/serial.cpp
@@ -21,36 +21,39 @@ double get2ndMin(double *candidates){
 }
 
 
-double* verification(double **A, int n){
+void verification(double *A, int n){
     double v1, v2,v3;
-    double *result;
     v1 = 0.0;
     for(int i=0; i<n; i++){
         for(int j=0; j<n; j++){
-            v1 += A[i][j];
+            v1 += A[i*n+j];
         }
     }
 
     int fl = floor((double)n/2);
-    v2 = A[fl][fl];
-    v3 = A[37][47];
+    v2 = A[fl*n+fl];
+    v3 = A[37*n+47];
     // __syncthreads();
-    result[0] = v1;
-    result[1] = v2;
-    result[2] = v3; 
-
-    return result;
+    A[0] = v1;
+    A[1] = v2;
+    A[2] = v3; 
 }
 
 
 int main(int argc, char** argv){
 	int n = atoi(argv[1]);
-	static double** array;
-	// double ** array = new double*[n]; 
-	// for(int i=0; i<n; i++){array[i] = new double[n]; }
+	int N = n*n;
+
+	int size  = N*sizeof(double);
+
+	double *A;
+	double *prev_A;
+	A = (double *) malloc(size);
+	prev_A = (double *) malloc(size);
     for(int i =0; i<n;i++){
         for(int j =0; j<n; j++){
-            array[i][j] = pow(1+cos(2*i)+sin(j),2);
+            A[i*n+j] = pow(1+cos(2*i)+sin(j),2);
+            prev_A[i*n+j] = pow(1+cos(2*i)+sin(j),2);
         }
     }
 
@@ -58,36 +61,48 @@ int main(int argc, char** argv){
 
     int t  = 10; 
     
-    static double **tmp = array;
-
     for(int e = 0; e<t; e++){ // 10 iteration
     	for(int i = 0; i<n; i++){
     		for(int j=0; j<n; j++){
-
     			if(i==0 || i==n-1 || j==0 || j==n-1){
-    				array[i][j] = tmp[i][j];
+    				A[i*n+j] = prev_A[i*n+ j];
     			}
-    			double candidates[] ={array[i+1][j+1], array[i+1][j-1], array[i-1][j-1], array[i-1][j+1]};
+    			double candidates[] ={A[(i+1)*n+ (j+1)], A[(i+1)*n +(j-1)], A[(i-1)*n + j-1], A[(i-1)*n + j+1]};
 
-    			array[i][j] = tmp[i][j] + get2ndMin(candidates);
+    			A[i*n+j] = prev_A[i*n+j] + get2ndMin(candidates);
     		}
+    		// printf("i %d\n", i);
+
     	}
 
-    	double **xx = array;
-    	tmp = array;
-    	array = xx;
+    	
+    	double *xx = A;
+    	A = prev_A;
+    	prev_A = xx;
+    }
+    // verification(prev_A, n);
+    double v1, v2,v3;
+    v1 = 0.0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            v1 += prev_A[i*n+j];
+        }
     }
 
-    double *result;
-    result =(double *)malloc(3*sizeof(double));
-    result = verification(array, n);
+    int fl = floor((double)n/2);
+    v2 = prev_A[fl*n+fl];
+    v3 = prev_A[37*n+47];
+    // __syncthreads();
+    prev_A[0] = v1;
+    prev_A[1] = v2;
+    prev_A[2] = v3; 
+
+
     double endtime = time(0);
 
-
     printf ("Time for the kernel: %f s\n", (starttime - endtime));
-    printf("verisum all %f\n", result[0]);
-    printf("verification n/2 %f\n", result[1]);
-    printf("verification A[37][47] %f\n", result[2]);
+    printf("verisum all %f\n", prev_A[0]);
+    printf("verification n/2 %f\n", prev_A[1]);
+    printf("verification A[37][47] %f\n", prev_A[2]);
 
-    free(result);
 }
