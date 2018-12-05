c2dccac9dd45ca6256e22385ba994ba6cc54da42
Ethan <zhangzhbd@gmail.com>
2018-12-05 17:08:20 -0500

    problem setup for project

---
+++ rl_parallel/proj.cpp
@@ -0,0 +1,19 @@
+//massive asynchronous reinforcement learning sarsa, q-learning and A3C. 
+// problems to solve:
+// is asynchronous good? all sychornized agents work better in massive parallelism status. 
+
+#include <iostream>
+#include <stdlib.h>
+#include <cmath>
+#include <ctime>
+#include <cuda.h>
+#include <float.h>
+
+#define THREADS_PER_DIM 32
+using namespace std;
+
+
+int main(int argc, char** argv){
+
+	return 0;
+}
