2d0db62b12c3c8a9f0c49d51d5994499ba77b928
Ethan <zhangzhbd@gmail.com>
2019-03-04 17:23:17 -0500

    read file success

--- flux_itsc/executableRead.cpp
+++ flux_itsc/executableRead.cpp
@@ -13,31 +13,52 @@
 using namespace std; 
 
 int main(int argc, char** argv){
-	int m= atoi(argv[1]);
-	string fileA, fileB; 
-	fileA = atoi(argv[2]);
-	fileB = atoi(argv[3]);
+	// int m= atoi(argv[1]);
+	cout<<"starting"<<endl;
+	// string fileA, fileB; 
+	// fileA = atoi(argv[1]);
+	// fileB = atoi(argv[2]);
+	// printf("here");
+	// cout<<fileA<<endl;
+	// cout<<fileB<<endl;
+	// string fileA("A.txt");
+	// string fileB("B.txt");
+	ifstream mA;
 
-	ifstream mA, mB;
-	mA.open(fileA); 
-	mB.open(fileB); 
+	printf("%s\n", "opening file");
+	// cout<<fileA<<endl;
+	// cout<<fileB<<endl;
 
+	mA.open("/Users/ethan/Documents/repo/parallel/flux_itsc/A.txt"); 
+	if (!mA) {
+    cout << "Unable to open file A.txt\n";}
+  // call system to stop
+    cout<<mA<<endl;
+
+	ifstream mB;
+	mB.open("/Users/ethan/Documents/repo/parallel/flux_itsc/B.txt"); 
+	if (!mB) {
+    cout << "Unable to open file B.txt\n";}
+    cout<<mB<<endl;
+	printf("%s\n", "file read");
+	
 	std::vector<double>numbers;
 	double number;
-	while(mA >> number){
-    numbers.push_back(number);}
+	while(mA >> number){numbers.push_back(number);}
+    printf("%s\n", "get A");
+    if(numbers.empty()){cout<<"empty\n";}
+    // cout<<numbers[0]<<numbers[1]<<numbers[2]<<endl;
+    cout<<numbers.size()<<'\n';
+    cout<<endl;
 
     std::vector<int>indexes;
     int index;
     while(mB >> index){
     	indexes.push_back(index);
     }
-
-    printf("top3A %f, %f, %f \n", numbers[0], numbers[1], numbers[2]);
-    cout<<numbers.size()<<'\n';
-    cout<<endl;
-
-    printf("top3B %d, %d, %d  \n", indexes[0], indexes[1], indexes[2]);
+    printf("%s\n", "get B");
+    // printf("top3B %d, %d, %d  \n", indexes[0], indexes[1], indexes[2]);
     cout<<indexes.size()<<'\n';
+    return 0;
 
 }
